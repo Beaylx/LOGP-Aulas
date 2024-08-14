6dd19a9c7675202c0cde2c339ef8b61093883f6d
Beaylx <lfs.beatriz.leonardo@gmail.com>
2024-08-14 17:04:36 -0300

    Aula

---
+++ Aula Python/exemplo laço.py
@@ -0,0 +1,4 @@
+for i in range (0, 200000):
+    print(i)
+    if 1 == 5:
+        break
---
+++ Listas de exercícios em C/Listas 3 - Sala/Exe 3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    float n, cont, x;
+    printf("Digite um valor para tabuada: ")
+    scanf((""))
+}
