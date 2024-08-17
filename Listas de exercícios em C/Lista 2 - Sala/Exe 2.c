6f5fea8997c6320be4a2f34a991aa25191f6a851
Beaylx <lfs.beatriz.leonardo@gmail.com>
2024-08-16 23:03:00 -0300

    Ajustes (Commit certo)

--- Listas de exercícios em C/Lista 2 - Sala/Exe 2.c
+++ Listas de exercícios em C/Lista 2 - Sala/Exe 2.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
-int main()
-{
+
+int main() {
     float N1, N2, M, E, NV;
+
     printf("Insira sua primeira nota: ");
     scanf("%f", &N1);
-    printf("Insira sua segunda nora: ");
+    printf("Insira sua segunda nota: ");
     scanf("%f", &N2);
-    M=(N1=N2)/2;
-    if (M >= 6.0)
-    printf("Parabén! você foi aprovado com a média: %2f", M);
-    else
-    printf("Você não foi aprovado! Insira sua nota do exame: ");
-    scanf("%f", E);
-    NV=(N1+N2+E)/2;
-    if (NV >= 5.0)
-    printf("Você foi aprovado com média: %2f", NV);
-    else
-    printf("Você não foi aprovado");
+
+    M = (N1 + N2) / 2;
+
+    if (M >= 6.0) {
+        printf("Parabéns! Você foi aprovado com a média: %.2f\n", M);
+    } else {
+
+        printf("Você não foi aprovado! Insira sua nota do exame: ");
+        scanf("%f", &E);
+
+        NV = (M + E) / 2;
+
+        if (NV >= 5.0) {
+            printf("Você foi aprovado com a média: %.2f\n", NV);
+        } else {
+            printf("Você não foi aprovado\n");
+        }
+    }
 
     return 0;
 }
